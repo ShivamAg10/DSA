667367f5a4da8ad131e142a60c6e3d0c80641a1b
Shivam1848 <kashokagrawal1848@gmail.com>
2025-01-14 23:44:52 +0530

    some changes in function

--- Arrays/Leetcode_2239.py
+++ Arrays/Leetcode_2239.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findClosestNumber(self, nums: List[int]) -> int:
+    def findClosestNumber(self, nums) -> int:
         closest = nums[0]
         for i in nums:
             if abs(i) < abs(closest):
