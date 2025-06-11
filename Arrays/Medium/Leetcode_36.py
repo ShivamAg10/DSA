569b10ab8f747b6e61570d39cc8f2b25c2e1f929
Shivam1848 <kashokagrawal1848@gmail.com>
2025-06-11 05:57:26 +0530

    Valid Sudoku

---
+++ Arrays/Medium/Leetcode_36.py
@@ -0,0 +1,112 @@
+class Solution:
+    def isValidSudoku(self, board):
+        for i in range(9):
+            rowNumbers = {"1","2","3","4","5","6","7","8","9"}
+            colNumbers = {"1","2","3","4","5","6","7","8","9"}
+            boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+            for j in range(9):
+                if board[i][j] in rowNumbers: #board[i][j] != ".":
+                    rowNumbers.discard(board[i][j])
+                    # print(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+
+                if board[j][i] in colNumbers: #board[i][j] != ".":
+                    colNumbers.discard(board[j][i])
+                elif board[j][i] == ".":
+                    pass
+                else:
+                    return False
+        
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(3):
+            for j in range(3,6):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(3):
+            for j in range(6,9):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(3,6):
+            for j in range(3):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(3,6):
+            for j in range(3,6):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(3,6):
+            for j in range(6,9):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(6,9):
+            for j in range(3):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(6,9):
+            for j in range(3,6):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:
+                    return False
+        boxNumbers = {"1","2","3","4","5","6","7","8","9"}
+        for i in range(6,9):
+            for j in range(6,9):
+                if board[i][j] in boxNumbers: #board[i][j] != ".":
+                    boxNumbers.discard(board[i][j])
+                elif board[i][j] == ".":
+                    pass
+                else:   
+                    return False
+        return True
+
+# Or
+
+starts = [(0, 0), (0, 3), (0, 6),
+                (3, 0), (3, 3), (3, 6),
+                (6, 0), (6, 3), (6, 6)]
+
+for i,j in starts:
+    print(i,j)
