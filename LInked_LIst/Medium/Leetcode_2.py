67a55b356095e79952b5166b71a4dc888116388d
Shivam1848 <kashokagrawal1848@gmail.com>
2025-06-25 15:07:43 +0530

    Linked List Leetcode #2

---
+++ LInked_LIst/Medium/Leetcode_2.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1,l2):
+        head = ListNode(0)
+        root = head
+        carry = 0
+
+        while l1 or l2:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            s = v1 + v2 + carry
+
+            carry = s // 10
+            s %= 10
+
+            head.next = ListNode(s)
+            head = head.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry:
+            head.next = ListNode(carry)
+        
+        return root.next
+        
